f101d884b156a22810004288a4319043dcf4c711
ckl <nanji@daikyn.com>
2026-08-04 17:46:55 +0800

    style: 克数显示精确到小数点后 4 位（积存金生息小数）

--- goldpulse/lib/utils/formatters.dart
+++ goldpulse/lib/utils/formatters.dart
@@ -3,7 +3,8 @@ import 'package:intl/intl.dart';
 import '../constants/app_theme.dart';
 
 final _number = NumberFormat('#,##0.00');
-final _grams = NumberFormat('#,##0.00');
+// 克数需精确到小数点后 4 位（积存金生息常出现 0.0825g 级小数）
+final _grams = NumberFormat('#,##0.0000');
 final _money = NumberFormat('#,##0.00');
 
 String fmtPrice(double v) => _number.format(v);
